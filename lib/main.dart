2340c7a1b36316a13e72a7f864d8ec2f50bbe234
Mukhammadjon <sheykxmuhammad@gmail.com>
2023-05-29 19:53:45 +0500

    ss

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'N-hisob',
+      title: 'N-ss',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
